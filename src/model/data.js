eedf02baf141683c5f8ac4c719664ee5dd53116a
Tracysshelton <tracy@zaronology.com>
2024-02-06 22:43:45 -0700

    home page has been updated with a custom button

--- src/model/data.js
+++ src/model/data.js
@@ -12,3 +12,87 @@ export const sliderData = [
         image: require('../assets/images/photo.jpg'),
     },
 ];
+
+export const latestChef = [
+    {
+        id: 1,
+        title: 'Recipe one',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+    {
+        id: 2,
+        title: 'Recipe Two',
+        subtitle: 'test',
+        image: require('../assets/images/i2G.png'),
+    },
+    {
+        id: 3,
+        title: 'Recipe Three',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+    {
+        id: 4,
+        title: 'Recipe Three',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+    {
+        id: 5,
+        title: 'Recipe Three',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+    {
+        id: 6,
+        title: 'Recipe Three',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+];
+
+export const yourchef = [
+    {
+        id: 1,
+        title: 'Recipe one',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+    {
+        id: 2,
+        title: 'Recipe Two',
+        subtitle: 'test',
+        image: require('../assets/images/i2G.png'),
+    },
+    {
+        id: 3,
+        title: 'Recipe Three',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+    {
+        id: 4,
+        title: 'Recipe Three',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+    {
+        id: 5,
+        title: 'Recipe Three',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+    {
+        id: 6,
+        title: 'Recipe Three',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+    {
+        id: 7,
+        title: 'Recipe Three',
+        subtitle: 'test',
+        image: require('../assets/images/photo.jpg'),
+    },
+];
